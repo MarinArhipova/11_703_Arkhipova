13858b3b4980ca18f63946371dc47f1c16760f34
marina.arkhipova <marina.arkhipova@simbirsoft.com>
2020-09-18 01:22:28 +0300

    add task №1

--- task_1/main.py
+++ task_1/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def count_age(train_dataset):
+def build_histogram_of_survival_by_age(train_dataset):
     plt.hist(
         x=[train_dataset[train_dataset['Survived'] == 1]['Age'], train_dataset[train_dataset['Survived'] == 0]['Age']],
         stacked=True, color=['b', 'r'], label=['Survived', 'Dead'])
@@ -12,6 +12,17 @@ def count_age(train_dataset):
     plt.show()
 
 
+def build_histogram_of_survival_by_class(train_dataset):
+    plt.hist(x=[train_dataset[train_dataset['Survived'] == 1]['Pclass'], train_dataset[train_dataset['Survived'] == 0]['Pclass']],
+             stacked=True, color=['b', 'r'], label=['Survived', 'Dead'])
+    plt.title('Histogram')
+    plt.xlabel('Class')
+    plt.ylabel('Number of Passengers')
+    plt.show()
+
+
 if __name__ == '__main__':
     train_dataset = pd.read_csv("../dataset/train.csv")
-    count_age(train_dataset)
+    build_histogram_of_survival_by_age(train_dataset)
+    build_histogram_of_survival_by_class(train_dataset)
+
